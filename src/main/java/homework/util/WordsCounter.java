68d35c47d46057759b07a6ec46e2b8d52a4cda43
Кузнецов Алексей Эдуардович <akuznetsov@mpl.lc>
2021-09-27 15:11:13 +0300

    change AtomicInteger to AtomicLong

--- src/main/java/homework/util/WordsCounter.java
+++ src/main/java/homework/util/WordsCounter.java
@@ -5,18 +5,19 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
 
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 
 public class WordsCounter {
-    private Converter converter = new Converter();
+    private final Converter converter = new Converter();
 
     public void count(ArrayList<String> list, String root) {
         ArrayList<String> words = converter.toList(list);
         HashMap<String, Long> frequencyMap = converter.toMap(words);
-        AtomicInteger count = new AtomicInteger();
+        AtomicLong count = new AtomicLong();
 
         frequencyMap
                 .entrySet()
@@ -26,7 +27,7 @@ public class WordsCounter {
                 .forEach(k -> {
                     if (k.getKey().contains(root)) {
                         System.out.printf("%s = %d %n", k.getKey(), k.getValue());
-                        count.getAndAdd(Math.toIntExact(k.getValue()));
+                        count.getAndAdd(k.getValue());
                     }
                 });
         System.out.println("TOTAL: " + count);
